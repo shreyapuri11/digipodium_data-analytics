00fbe88bb59517c99e7c4411046e7364143892d4
shreya <shreyapuribbk@gmail.com>
2023-08-19 14:37:08 +0530

    data_analysis

---
+++ data_analysis/dapp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+# run in terminal
+# cd data_analysis
+# streamlit run dapp.py
+
+#reactive apps
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import seaborn as sns
+
+st.title('Data Analysis App')
